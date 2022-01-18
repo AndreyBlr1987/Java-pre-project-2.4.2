37ab8119f154eb01f0e6d1eb6c32b519c970723a
Melnik Andrey <melnik_blr1987@mail.ru>
2022-01-18 14:11:21 +0300

    Merge remote-tracking branch 'origin/master'

---
+++ target/Spring_MVC/WEB-INF/classes/data.sql
@@ -0,0 +1,10 @@
+
+INSERT INTO users VALUES (1, 'ivan', 'ivanov', 'Vanya', '$2a$04$jSO6ibDUkdofTqx1nEfe6uaVqb2Ks3M0UwtlfZOX.VDc45UumELqK');
+INSERT INTO users VALUES (2, 'Stas', 'Stasov', 'Stas', '$2a$04$jSO6ibDUkdofTqx1nEfe6uaVqb2Ks3M0UwtlfZOX.VDc45UumELqK');
+
+
+INSERT INTO roles VALUES (1, 'USER');
+INSERT INTO roles VALUES (2, 'ADMIN');
+
+INSERT INTO users_roles VALUES (1, 1);
+INSERT INTO users_roles VALUES (2, 2);
