fed86a946ff839ff66fe34114ace862f919f6c79
Melnik Andrey <melnik_blr1987@mail.ru>
2022-01-18 14:14:55 +0300

    Merge remote-tracking branch 'origin/master'

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 
-INSERT INTO users VALUES (1, 'ivan', 'ivanov', 'Vanya', '$2a$04$jSO6ibDUkdofTqx1nEfe6uaVqb2Ks3M0UwtlfZOX.VDc45UumELqK');
+INSERT INTO users VALUES (1, 'Vanya', 'ivanov', 'ivan', '$2a$04$jSO6ibDUkdofTqx1nEfe6uaVqb2Ks3M0UwtlfZOX.VDc45UumELqK');
 INSERT INTO users VALUES (2, 'Stas', 'Stasov', 'Stas', '$2a$04$jSO6ibDUkdofTqx1nEfe6uaVqb2Ks3M0UwtlfZOX.VDc45UumELqK');
 
 
